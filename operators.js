37470bced8aa8771dc0a30355593cd7412c11483
malaniharshita <malaniharshita@gmail.com>
2024-03-18 15:46:05 +0530

    arrays and spread operators

---
+++ operators.js
@@ -0,0 +1,21 @@
+// let array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// let modifyArray = array.map((item) => {
+//     if(item == ' '){
+//         return 'empty string';
+//     }
+//     else{
+//         return item;
+//     }
+// })node
+// console.log(modifyArray[2]);
+// console.log(array);
+
+//hobbies const and it still doesnt error out when we add new elements to the array
+const hobbies = ['Sport','Cooking'];
+hobbies.push('Programming');
+console.log(hobbies);
+
+// const array1 = [1,2,3,4,5];
+// const array2 = [...array1,6];
+// console.log(array1);
+// console.log(array2);s
