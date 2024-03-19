45b84d39cd499508caae42d42feb274e9bc44d8b
malaniharshita <malaniharshita@gmail.com>
2024-03-19 10:28:09 +0530

    serverCreated

---
+++ creatServer.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port=4000;
+const name = 'harshita'
+//const hostname = localhost;
+const server = http.createServer((req,res) => {
+    console.log(name);
+    res.end(name);
+    process.exit();
+});
+
+server.listen(port);
